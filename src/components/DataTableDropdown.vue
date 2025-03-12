f1f1222cf0c30b5d21660ea21febc6f8ba21f10d
daviidii <davidxduran05@gmail.com>
2025-03-13 00:11:16 +0800

    user table

---
+++ src/components/DataTableDropdown.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { MoreHorizontal } from 'lucide-vue-next'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+defineProps<{
+  userId: number
+}>()
+
+const copy = (id: string) => {
+  navigator.clipboard.writeText(id)
+}
+
+const goToUserUpdate = (id: string) => {
+  router.push(`/user/update/${id}`)
+}
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" class="w-8 h-8 p-0">
+        <span class="sr-only">Open menu</span>
+        <MoreHorizontal class="w-4 h-4" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+      <DropdownMenuItem @click="copy(userId.toString())"> Copy user ID </DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem @click="goToUserUpdate(userId.toString())">Update User</DropdownMenuItem>
+      <DropdownMenuItem>Delete User</DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
--- src/pages/AddUser.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts">
-import PageWrapper from '@/components/PageWrapper.vue'
-</script>
-<template>
-  <PageWrapper>Add user</PageWrapper>
-</template>
